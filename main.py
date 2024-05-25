21ac1f08001e29550ddeaf5bf2ad36ad0d4207f3
lsn12 <lindsesn@uci.edu>
2024-05-25 11:11:09 -0700

    Asks the user for a city input and main() uses location as a parameter to input the city into the API url to look for restaurants

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 import requests
 import json
 
-API_url = 'https://api.yelp.com/v3/businesses/search?location=Irvine'
+API_url = ''
 headers = {"Authorization": "Bearer SU2UrP6AOSyh1kzid0InZsT_QrnCw71FYgT9O1goVd1481qOD5sCkRKkwpUMB6ZlyzLnw--TJCiII7w0ywF6HKsaTsS046M8TZvZwAlFY75MHSBBoyPxMuI2exVSZnYx", "accept": "application/json"}
 
-def main():
+def main(location):
 
-    print('Welcome to EatCents!')
+    API_url = f'https://api.yelp.com/v3/businesses/search?location={location}'
     parameter = {'limit': 50}
-    response = requests.get(url=API_url, headers=headers, params=parameter)
 
     try:
+        response = requests.get(url = API_url, headers = headers, params = parameter)
+
         if response.ok:
             data = response.json()
             print(data)
 
         for business in data['businesses']:
             try:
-                if business['price'] == '$':
+                if business['price'] == '$' or business['price'] == '$$':
                     print(f"{business['name']}: {business['price']}")
             except KeyError: # KeyError triggers if business does not have a price level shown
                 pass
-                # print(f"{business['name']}: no price is shown")
+
     except:
         print('400 Bad Request')
 
 
 if __name__ == "__main__":
-    main()
+    print("Welcome to Eatcents !! ")
+    city = input('What city would you like to search?: ')
+    main(city)
