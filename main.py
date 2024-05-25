e72f5a0a3acced806f2be1ff6b883c7144b568ee
lsn12 <lindsesn@uci.edu>
2024-05-25 13:13:37 -0700

    changed storage format of business name, price, and coordinates

--- main.py
+++ main.py
@@ -1,54 +1,69 @@
 import requests
 import json
+import plotly.graph_objects as go
 
+
+# global variables
 API_url = ''
-headers = {"Authorization": "Bearer SU2UrP6AOSyh1kzid0InZsT_QrnCw71FYgT9O1goVd1481qOD5sCkRKkwpUMB6ZlyzLnw--TJCiII7w0ywF6HKsaTsS046M8TZvZwAlFY75MHSBBoyPxMuI2exVSZnYx", "accept": "application/json"}
+headers = {
+    "Authorization": "Bearer SU2UrP6AOSyh1kzid0InZsT_QrnCw71FYgT9O1goVd1481qOD5sCkRKkwpUMB6ZlyzLnw--TJCiII7w0ywF6HKsaTsS046M8TZvZwAlFY75MHSBBoyPxMuI2exVSZnYx",
+    "accept": "application/json"}
 
+
+# functions
 def main(location):
-
     API_url = f'https://api.yelp.com/v3/businesses/search?location={location}'
     parameter = {'limit': 50}
+    main_set = []
+    Restuarant = namedtuple('Restaurant', ['name', 'price', 'coordinates'])
 
     try:
         response = requests.get(url = API_url, headers = headers, params = parameter)
 
         if response.ok:
             data = response.json()
-            print(data)
+            # print(data)
             print("================")
 
-        sorted_restaurant = {}
-        sorted_restaurant_loc_dict = {}
+        # sorted_restaurant = {}
+        # sorted_restaurant_loc_dict = {}
         latitude_list = []
         longitude_list = []
 
-
         for business in data['businesses']:
+            c_name = ''
+            c_price = ''
+            c_coord = {}
+
             try:
                 if business['price'] == '$':
-                    sorted_restaurant[business['name']] = business['price']
-                    sorted_restaurant_loc_dict[business['name']] = business['coordinates']
-                    #print(sorted_restaurant_loc_dict[business['name']])
-                if business['price'] == '$$':
-                    sorted_restaurant[business['name']] = business['price']
-                    sorted_restaurant_loc_dict[business['name']] = business['coordinates']
-                    # print(sorted_restaurant_loc_dict[business['name']])
-                    #print(f"{business['name']}: {business['price']}")
+                    c_name = business['name']
+                    c_price = business['price']
+                    c_coord = business['coordinates']
+                    # c_rating = business['rating']
+                    r_tuple = Restuarant(c_name, c_price, c_coord)
+                    main_set.append(r_tuple)
 
-            except KeyError: # KeyError triggers if business does not have a price level shown
+
+                elif business['price'] == '$$':
+                    c_name = business['name']
+                    c_price = business['price']
+                    c_coord = business['coordinates']
+                    # c_rating = business['rating']
+                    r_tuple = Restuarant(c_name, c_price, c_coord)
+                    main_set.append(r_tuple)
+
+            except KeyError:
                 pass
 
-        print(f"Restaurants sorted from $ to $$ in {location}: \n\n{sorted_restaurant}")
-        # print(sorted_restaurant_loc_dict)
+        print(main_set)
 
-        for rest_name in sorted_restaurant_loc_dict:
-            latitude_list.append(rest_name['latitude'])
-            longitude_list.append(rest_name['longitude'])
-        print(latitude_list)
-        print(longitude_list)
-
-
-        #making_map(name=name, lat=sorted_restaurant_loc_dict, lon=sorted_restaurant_loc_dict)
+        # latitude and longitude lists
+        for rest_name in main_set:
+            latitude_list.append(rest_name.coordinates['latitude'])
+            longitude_list.append(rest_name.coordinates['longitude'])
+        # print(latitude_list)
+        # print(longitude_list)
 
 
     except:
