ab54438e68f4ed502e6fe408806992527ce8ecf8
lsn12 <lindsesn@uci.edu>
2024-05-25 17:53:24 -0700

    updated map.py that takes the user to a link of the map

--- map.py
+++ map.py
@@ -1,34 +1,52 @@
 #adding map
-import plotly.graph_objects as go
 
+import plotly.graph_objects as go
 token = 'pk.eyJ1IjoibmF0aGFsaWVjaGl1dSIsImEiOiJjbHdtamZmcXUxdWpwMml0Z2pxc3Znc3A2In0.YudR05400cNQ08lFk67_Jw'
-def making_map(name, lat, lon):
+
+def set_color(price):
+    color_list = []
+    for p in price:
+        if p == '$':
+            color_list.append("skyblue")
+        elif p == '$$':
+            color_list.append("pink")
+    return color_list
+
+
+def making_map(name, lat, lon, price):
+
     mapbox_access_token = token
+
     fig = go.Figure(go.Scattermapbox(
-        style = 'basic',
         lat = lat,
         lon = lon,
         mode = 'markers',
         marker = go.scattermapbox.Marker(
-            size = 11
+            size = 10,
+            color = set_color(price),
+            opacity = 0.75
         ),
-        text = name
+        text = name,
+        customdata = price,
+        hovertemplate = 'Restaurant: %{text}'+'<br>Coordinate: (%{lat},%{lon})'+'<br>Price: %{customdata}'
     ))
+
+    #hovertemplate = 'Price: %{price}'+'<br>Coordinate: %{lat}%{lon}'
 
     fig.update_layout(
         autosize = True,
         hovermode = 'closest',
         mapbox=dict(
             accesstoken= mapbox_access_token,
+            style = 'dark',
             bearing=0,
             center=dict(
                 lat= lat[0],
                 lon= lon[0],
             ),
             pitch=0,
-            zoom=10,
+            zoom=11,
         ),
-
-
     )
+
     fig.show()
